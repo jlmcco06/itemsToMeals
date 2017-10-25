651c5b7a1758069906a8a433d5d9815187c4cbc9
Josh L McCollum <joshlmccollum@gmail.com>
2017-10-25 12:52:49 -0400

    Updated model.icons array with titles
    Added titles to the model.icons array

--- animations.js
+++ animations.js
@@ -1,16 +1,23 @@
 var model = {
 	icons : [
-	{image: "images/coffee.png",
-	banner: "For the price of the average cup of coffee Philabunace can provide 5 meals to neighbors in need! Make it a latte and it's 7 meals!",
-	meals: "5 to 7 meals!"},
-	{image: "images/chefHat.png",
-	banner: "For the cost on the average Philabunace 'dinner out' Philabunace can feed 60 hungery people!",
-	meals: "60 meals"},
-	{image:"images/vacation.png",
-	banner: "For less than the cost of a family vacation, you can change a life, by sponsoring a student at teh Philabundance Community Kitchen!"}
+	{
+		title : "coffee",
+		image: "images/coffee.png",
+		banner: "For the price of the average cup of coffee Philabunace can provide 5 meals to neighbors in need! Make it a latte and it's 7 meals!",
+		meals: "5 to 7 meals!"},
+	{
+		title: "dinner-out",
+		image: "images/chefHat.png",
+		banner: "For the cost on the average Philabunace 'dinner out' Philabunace can feed 60 hungery people!",
+		meals: "60 meals"},
+	{
+		title: "vacation",
+		image:"images/vacation.png",
+		banner: "For less than the cost of a family vacation, you can change a life, by sponsoring a student at teh Philabundance Community Kitchen!",
+		meals: "priceless"}
 	]
 }
-console.log(model.icons[0].image);
+
 var viewModel = {
 	iconsArray : ko.observableArray(),
 
@@ -21,7 +28,6 @@ var viewModel = {
 			icon = model.icons[i].image;
 			icons.push(icon);
 		}
-		console.log(icons)
 		this.iconsArray(icons);
 	}
 }
